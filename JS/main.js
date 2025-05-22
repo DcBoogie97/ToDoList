71d3b9907a86889337cd896e0e8e762561372f24
Khanh Nguyen <khanhnd.work0311@gmail.com>
2025-05-22 16:09:50 +0700

    test failed

--- JS/main.js
+++ JS/main.js
@@ -28,41 +28,41 @@ function addToDo(event) {
     // Prevents form from submitting / Prevents form from relaoding;
     event.preventDefault();
 
-    // toDo DIV;
-    const toDoDiv = document.createElement("div");
-    toDoDiv.classList.add('todo', `${savedTheme}-todo`);
+    // // toDo DIV;
+    // const toDoDiv = document.createElement("div");
+    // toDoDiv.classList.add('todo', `${savedTheme}-todo`);
 
-    // Create LI
-    const newToDo = document.createElement('li');
-    if (toDoInput.value === '') {
-            alert("You must write something!");
-        } 
-    else {
-        newToDo.innerText = "hey";
-        // newToDo.innerText = toDoInput.value;
-        newToDo.classList.add('todo-item');
-        toDoDiv.appendChild(newToDo);
+    // // Create LI
+    // const newToDo = document.createElement('li');
+    // if (toDoInput.value === '') {
+    //         alert("You must write something!");
+    //     } 
+    // else {
+    //     // newToDo.innerText = "hey";
+    //     newToDo.innerText = toDoInput.value;
+    //     newToDo.classList.add('todo-item');
+    //     toDoDiv.appendChild(newToDo);
 
-        // Adding to local storage;
-        savelocal(toDoInput.value);
+    //     // Adding to local storage;
+    //     savelocal(toDoInput.value);
 
-        // check btn;
-        const checked = document.createElement('button');
-        checked.innerHTML = '<i class="fas fa-check"></i>';
-        checked.classList.add('check-btn', `${savedTheme}-button`);
-        toDoDiv.appendChild(checked);
-        // delete btn;
-        const deleted = document.createElement('button');
-        deleted.innerHTML = '<i class="fas fa-trash"></i>';
-        deleted.classList.add('delete-btn', `${savedTheme}-button`);
-        toDoDiv.appendChild(deleted);
+    //     // check btn;
+    //     const checked = document.createElement('button');
+    //     checked.innerHTML = '<i class="fas fa-check"></i>';
+    //     checked.classList.add('check-btn', `${savedTheme}-button`);
+    //     toDoDiv.appendChild(checked);
+    //     // delete btn;
+    //     const deleted = document.createElement('button');
+    //     deleted.innerHTML = '<i class="fas fa-trash"></i>';
+    //     deleted.classList.add('delete-btn', `${savedTheme}-button`);
+    //     toDoDiv.appendChild(deleted);
 
-        // Append to list;
-        toDoList.appendChild(toDoDiv);
+    //     // Append to list;
+    //     toDoList.appendChild(toDoDiv);
 
-        // CLearing the input;
-        toDoInput.value = '';
-    }
+    //     // CLearing the input;
+    //     toDoInput.value = '';
+    // }
 
 }   
 
